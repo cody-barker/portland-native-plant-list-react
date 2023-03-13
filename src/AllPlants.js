e97a12f90ded27be61f641607e7000e9941bf5ab
Cody <codybarker.or@gmail.com>
2023-03-13 15:10:40 -0700

    changing let to const for plantsSorted

--- src/AllPlants.js
+++ src/AllPlants.js
@@ -3,7 +3,7 @@ import Plant from './Plant'
 
 function AllPlants({allPlants, compare}) {
 
-    let plantsSorted = [...allPlants].sort(compare)
+    const plantsSorted = [...allPlants].sort(compare)
     const allPlantsComps = plantsSorted.map(plant => <Plant plant={plant} key={plant.id}/>)
     
     return(
