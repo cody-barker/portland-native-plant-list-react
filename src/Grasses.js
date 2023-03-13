71fa5f5a35ab9f3c4d65c6c287a2fbed12f4bf01
Cody <codybarker.or@gmail.com>
2023-03-13 14:43:31 -0700

    sorting comps in herbs and grasses

--- src/Grasses.js
+++ src/Grasses.js
@@ -4,7 +4,18 @@ import Plant from './Plant'
 function Grasses({allPlants}) {
 
     const grasses = allPlants.filter(plant => plant.type.toLowerCase().includes("grass"))
-    const grassComps = grasses.map(plant => <Plant plant={plant} key={plant.id}/>)
+    const grassesSorted = [...grasses].sort(compare)
+    const grassComps = grassesSorted.map(plant => <Plant plant={plant} key={plant.id}/>)
+    function compare(a, b) {
+        if (a.binomialName < b.binomialName) {
+            return -1
+        }
+        if (a.binomialName > b.binomialName) {
+            return 1
+        }
+        return 0
+    }
+    
 
     return(
         <div>
--- src/Herbs.js
+++ src/Herbs.js
@@ -4,7 +4,17 @@ import Plant from './Plant'
 function Herbs({allPlants}){
 
     const herbs = allPlants.filter(plant => plant.type.toLowerCase().includes("herb"))
-    const herbComps = herbs.map(plant => <Plant plant={plant} key={plant.id}/>)
+    const herbsSorted = [...herbs].sort(compare)
+    const herbComps = herbsSorted.map(plant => <Plant plant={plant} key={plant.id}/>)
+    function compare(a, b) {
+        if (a.binomialName < b.binomialName) {
+            return -1
+        }
+        if (a.binomialName > b.binomialName) {
+            return 1
+        }
+        return 0
+    }
 
     return(
         <div>
